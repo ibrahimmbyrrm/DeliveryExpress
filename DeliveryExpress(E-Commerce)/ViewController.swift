9a56cfb5415f04e3d869e7ee109c9cd2bba3a7ae
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-06-22 18:03:20 +0300

    Initial Commit

---
+++ DeliveryExpress(E-Commerce)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryExpress(E-Commerce)
+//
+//  Created by İbrahim Bayram on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
